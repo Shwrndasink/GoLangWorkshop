36883b0a737d86c6ebada6fae62cfb732df8b52d
Marc White <marcwhite@Marcs-MacBook-Air.local>
2024-08-22 19:18:36 -0600

    Introduction to routines and channels

---
+++ Types/data/workshop.go
@@ -0,0 +1,21 @@
+package data
+
+import "time"
+
+type Workshop struct {
+	Course
+	Instructor
+	Date time.Time
+	SignUpToCourse
+}
+
+func (c Workshop) SignUp() bool {
+	return true
+}
+
+func NewWorkshop(name string, instructor Instructor) Workshop {
+	w := Workshop{}
+	w.Name = name
+	w.Instructor = instructor
+	return w
+}
--- Types/main.go
+++ Types/main.go
@@ -11,4 +11,19 @@ func main() {
 	var joe data.Instructor = data.NewInstructor("Joe", "Martinez")
 	fmt.Println(joe.PrettyPrint())
 	fmt.Println(marc.PrettyPrint())
+
+	goCourse := data.Course{Id: 2, Name: "Go Fundamentals", Instructor: joe}
+
+	fmt.Printf("%v", goCourse)
+
+	swiftWorkshop := data.NewWorkshop("Switft with iOS", joe)
+	fmt.Printf("%v", swiftWorkshop)
+
+	var courses [2]data.SignUpToCourse
+	courses[0] = goCourse
+	courses[1] = swiftWorkshop
+
+	for _, course := range courses {
+		fmt.Println(course)
+	}
 }
